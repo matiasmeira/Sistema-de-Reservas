3062fec9b219564aa39991642adda0d08349ff26
Matias <matiasezequielmeira@gmail.com>
2026-03-05 02:17:09 -0300

    Entidad Reserva Creada

---
+++ src/main/java/com/matiasmeira/sistemadereservas/model/Reserva.java
@@ -0,0 +1,51 @@
+package com.matiasmeira.sistemadereservas.model;
+
+import java.security.Timestamp;
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+import java.time.LocalTime;
+
+import org.hibernate.annotations.CreationTimestamp;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+import jakarta.persistence.Table;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Entity
+@Table
+@Getter @Setter
+@AllArgsConstructor @NoArgsConstructor
+public class Reserva {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private LocalDate fechaReserva;
+    private LocalTime horaInicio;
+    private LocalTime horaFin;
+    private int duracionMinutos;
+    private String estado;
+    private double precioTotal;
+
+    @CreationTimestamp
+    @Column(updatable = false)
+    private LocalDateTime fechaCreacion;
+
+    @ManyToOne
+    @JoinColumn(name = "usuario_id")
+    private Usuario usuario;
+
+    @ManyToOne
+    @JoinColumn(name = "cancha_id")
+    private Cancha cancha;
+ 
+}
