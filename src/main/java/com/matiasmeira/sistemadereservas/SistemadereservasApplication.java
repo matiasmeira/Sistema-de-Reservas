364f4e14dbefc5c76c61c8b321c2addc6778703c
Matias <matiasezequielmeira@gmail.com>
2026-03-05 01:34:36 -0300

    estructura sistema

---
+++ src/main/java/com/matiasmeira/sistemadereservas/SistemadereservasApplication.java
@@ -0,0 +1,13 @@
+package com.matiasmeira.sistemadereservas;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SistemadereservasApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SistemadereservasApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/matiasmeira/sistemadereservas/SistemadereservasApplicationTests.java
@@ -0,0 +1,13 @@
+package com.matiasmeira.sistemadereservas;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SistemadereservasApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
