28e5d1bee53b12a02231334436ffcd3680efd5c4
Matias <matiasezequielmeira@gmail.com>
2026-03-05 02:17:01 -0300

    Entidad Cancha Creada

---
+++ src/main/java/com/matiasmeira/sistemadereservas/model/Cancha.java
@@ -0,0 +1,46 @@
+package com.matiasmeira.sistemadereservas.model;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+import jakarta.persistence.OneToMany;
+import jakarta.persistence.Table;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import java.util.List;
+
+@Entity
+@Table
+@Getter @Setter
+@AllArgsConstructor @NoArgsConstructor
+public class Cancha {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column (nullable = false)
+    private String nombre;
+
+    @Column (nullable = false)
+    private String tipo;
+
+    private double precioHora;
+    private String estado;
+    private String ubicacion;
+    private String descripcion;
+    
+    @ManyToOne
+    @JoinColumn(name = "usuario_id")
+    private Usuario usuario;
+
+    @OneToMany(mappedBy = "cancha")
+    private List<Reserva> reservas;
+    
+}
+
