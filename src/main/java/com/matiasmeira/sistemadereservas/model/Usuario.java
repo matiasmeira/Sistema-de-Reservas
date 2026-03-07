e4ef71b3c305628f012ad86b0488d5921628cc3e
Matias <matiasezequielmeira@gmail.com>
2026-03-07 15:00:55 -0300

    cambios en entidad Usuario para implementar Spring Security

--- src/main/java/com/matiasmeira/sistemadereservas/model/Usuario.java
+++ src/main/java/com/matiasmeira/sistemadereservas/model/Usuario.java
@@ -12,13 +12,19 @@ import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
+
+import java.util.Collection;
 import java.util.List;
+
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
 
 @Entity
 @Table
 @Getter @Setter
 @AllArgsConstructor @NoArgsConstructor
-public class Usuario {
+public class Usuario implements UserDetails {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
@@ -27,7 +33,7 @@ public class Usuario {
     private String email;
 
     @Column(nullable = false)
-    private String contraseña;
+    private String password;
 
     private String rol;
     private String nombre;
@@ -38,5 +44,25 @@ public class Usuario {
 
     @OneToMany(mappedBy = "usuario")
     private List<Establecimiento> establecimientos;
+
+    @Override
+    public Collection<? extends GrantedAuthority> getAuthorities() {
+        return List.of(new SimpleGrantedAuthority(rol));
+    }
+
+    @Override
+    public String getUsername() { return email; }
+
+    @Override
+    public boolean isAccountNonExpired() { return true; }
+
+    @Override
+    public boolean isAccountNonLocked() { return true; }
+
+    @Override
+    public boolean isCredentialsNonExpired() { return true; }
+
+    @Override
+    public boolean isEnabled() { return true; }
 
 }
